ae6cae971bfb80d672896f0d9e110bbc1c47080f
Elena <espanina94@gmail.com>
2026-03-08 11:12:46 +0300

    Реализовать Navigation Controller Push

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var tagLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBOutlet weak var exitButton: UIButton!
+    
+    
+    @IBAction func exitButtonAction(_ sender: UIButton) {
+    }
+}
---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+    
+    @IBOutlet weak private var imageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+    
+}
